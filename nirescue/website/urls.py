496893e1dce746ee1626f1f622bdfc0b3ccb2e46
Nicholas Ngeno (Techno Brain Global FZE) <nickngeno1@gmail.com>
2020-07-03 06:21:26 +0300

    commit

--- nirescue/website/urls.py
+++ nirescue/website/urls.py
@@ -4,4 +4,6 @@ from . import views
 # urlpatterns go her
 urlpatterns = [
     path('',views.home , name="home"),
+    path('about_us.html', views.about_us, name="about_us"),
+    path('blog.html', views.blog, name="blog"),
 ]
--- nirescue/website/views.py
+++ nirescue/website/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+# about us page
+def about_us(request):
+    return render(request, 'about_us.html')
+
+# blog page goes here
+def blog(request):
+    return render(request, 'blog.html')
